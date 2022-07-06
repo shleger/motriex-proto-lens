9b58ac84f6112fb7ebf4b5023f31ce237de2cb15
Andrey_Shleger <Andrey_Shleger@epam.com>
2022-07-07 00:30:31 +0300

    add import

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,10 @@
 module Main where
 
+import Data.ProtoLens (defMessage)
+import Data.ProtoLens.Labels ()
+import Lens.Micro
+import Lens.Micro.Extras (view)
+import qualified Proto.MarketData
+
 main :: IO ()
 main = putStrLn "Hello, Haskell!"
