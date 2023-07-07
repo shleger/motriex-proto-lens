f6db1fbf206e607afcfbcc8ee91fa1e808ec0c1e
Andrey_Shleger <Andrey_Shleger@epam.com>
2023-07-07 13:38:16 +0800

    toString enum, add time and output structure

--- src/Processing/Prices.hs
+++ src/Processing/Prices.hs
@@ -1,11 +1,13 @@
+{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE KindSignatures #-}
 {-# LANGUAGE OverloadedLabels #-}
-{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE PackageImports #-}
 {-# LANGUAGE UndecidableInstances #-}
 {-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE InstanceSigs #-}
 
 
 
@@ -25,28 +27,69 @@ import Network.GRPC.Client.Helpers (rawUnary)
 
 import Lens.Micro ((&), (.~), (^.))
 import Network.GRPC.ClientInit (runGrpc)
-import Data.ProtoLens (defMessage)
-import Data.ProtoLens.Field (field)
+import Data.ProtoLens.Message (defMessage)
 import Data.ProtoLens.Labels ()
+import Data.Int (Int64)
+import Proto.Marketdata (LastPrice)
+import Proto.Marketdata_Fields (figi)
+import Data.Text (Text) 
+import Data.Text as DT (unpack)
+import Proto.Google.Protobuf.Timestamp (Timestamp)
+
+import Data.Time.Clock.POSIX
+import Data.Time.Format.ISO8601
+import Data.Time (UTCTime)
+import qualified Proto.Google.Protobuf.Duration_Fields as PF
+import Data.Time.Clock (NominalDiffTime)
+import Data.Data (Typeable)
+
+import qualified Data.ByteString as T
+
 
 
 
 prices :: Helper.GrpcClient -> P.GetLastPricesRequest -> Client.ClientIO (Either Client.TooMuchConcurrency (RawReply P.GetLastPricesResponse))
-prices = rawUnary (PL.RPC :: PL.RPC P.MarketDataService "getLastPrices" )    
+prices = rawUnary (PL.RPC :: PL.RPC P.MarketDataService "getLastPrices" )
+
+
+data CurSymTin = BBG0013HJJ31 | BBG0013HGFT4 deriving (Enum, Typeable, Read)
+
+instance Show CurSymTin where
+  show :: CurSymTin -> String
+  show BBG0013HJJ31 = "EURRUB"
+  show BBG0013HGFT4 = "USDRUB"
+
+
 
 reqLastPrices2 :: P.GetLastPricesRequest
 reqLastPrices2 =
   defMessage
     & #figi .~ ["BBG0013HJJ31", "BBG0013HGFT4"] --EUR,USD
 
+data Currency = Currency { symbol :: !CurSymTin, value :: !String , time :: !UTCTime} deriving Show
+
+
+
+toCurrency :: LastPrice -> Currency
+toCurrency lp = do
+
+    let p1p2 = show p1 ++ "." ++ show p2 where
+            p1  = lp ^. PF.price . PF.units
+            p2  = fromIntegral $ lp ^. PF.price . PF.nano
+
+    Currency
+        { symbol = read $ DT.unpack $ lp ^. PF.figi,
+          value = p1p2,
+          time = posixSecondsToUTCTime $ fromIntegral $ lp ^. PF.time . PF.seconds
+        }
+
+
 
 lastPricesFetch::  Helper.GrpcClient -> IO()
 lastPricesFetch tinClient = do
     resp <- (^. PF.lastPrices) <$> runGrpc (prices tinClient reqLastPrices2)
 
-    let f = resp !! 1
-    let p1 = f ^. PF.price . PF.units
-    let p2 = fromIntegral $ f ^. PF.price . PF.nano
-    let p1p2 = show p1 ++ "." ++ show p2
+    let curs = map toCurrency resp
 
-    print (show p1p2)
+    -- TODO  wrap to json
+    print (show curs)
